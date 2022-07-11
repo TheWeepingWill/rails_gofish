340ceea3610c918bb82c2df19cda7002d4162686
William Jones <williamseejones793@gmail.com>
2022-07-11 12:02:01 -0400

    added my theme and nav bar components

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "user#new"
   get '/hello', to: 'application#hello'
   get '/goodbye', to: 'application#goodbye'
   get '/styleguide', to: 'styleguide#index'
