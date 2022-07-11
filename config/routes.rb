01c8858fbadf392c238074a1dede314becf990ba
William Jones <williamseejones793@gmail.com>
2022-07-11 14:23:23 -0400

    generated controller and models modified routs and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "user#new"
+  root "users#new"
   get '/hello', to: 'application#hello'
   get '/goodbye', to: 'application#goodbye'
   get '/styleguide', to: 'styleguide#index'
+  resources :users
 
 end
